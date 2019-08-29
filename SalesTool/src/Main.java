7874f861a8fb7452da171af20d4a91a60fcd424c
Steve Rittgers <rittgerss@wmpenn.edu>
2019-08-29 16:10:47 -0500

    revised woot message

--- SalesTool/src/Main.java
+++ SalesTool/src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 	private static void displayGreeting() {
 		System.out.println("HELLO HAPPY SALES PEOPLE!");
 		System.out.println("THIS APP SHOWS SALES DATA");
-		System.out.println("woot. it pushed");
+		System.out.println("woot. it pushed modified");
 	}
 
 }
